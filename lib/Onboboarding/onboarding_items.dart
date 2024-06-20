2d857f3d647da099a2eabdcc1202fa91fc201d30
Aldhyy2306 <mikelarief@gmail.com>
2024-06-20 22:46:20 +0700

    first commit

---
+++ lib/Onboboarding/onboarding_items.dart
@@ -0,0 +1,27 @@
+
+ import 'onboarding_info.dart';
+
+class OnboardingItems{
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: "NamaYgDibuat",
+        descriptions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+        image: "assets/onboarding1.gif"),
+
+    OnboardingInfo(
+        title: "supJudul1",
+        descriptions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+        image: "assets/onboarding2.gif"),
+
+    OnboardingInfo(
+        title: "supJudul2",
+        descriptions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+        image: "assets/onboarding3.gif"),
+
+    OnboardingInfo(
+        title: "supJudul1",
+        descriptions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+        image: "assets/onboarding4.gif"),
+
+  ];
+ }
